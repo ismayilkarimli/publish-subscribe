73e32ff4257250ad0837630ba1b87bebef8e29b1
ismayilkarimli <34961005+ismayilkarimli@users.noreply.github.com>
2020-12-16 01:42:41 +0400

    final version

--- publisher.py
+++ publisher.py
@@ -1,22 +1,22 @@
-import zmq, time, os, csv
+import zmq, time, os
+import subprocess as sp
 
 dir = input('Enter directory: ')
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB) # creating publisher socket
 
-file = open('config.csv', 'r') # config file
-reader = csv.reader(file)
-configs = [(ip, port) for row, (ip, port) in enumerate(reader) if row == 1]
+configs = open('config.txt', 'r') # config file
 
-ip = configs[0][0] # host ip
-port = configs[0][1] # what port to use
+ip = configs.readline().replace('\n', '') # host ip
+port = configs.readline() # what port to use
 p = 'tcp://' + ip + ':' + port # how to communicate
 socket.bind(p) # bind socket
 
-while True:
-    contents = os.listdir(dir) # gathering directory contents
 
+contents = os.listdir(dir)
+
+while True: # publisher will publish contents for 5 times
     for content in contents:
         socket.send_string('Folder ' + dir + ' has ' + content)
-    time.sleep(10) # waiting 10 seconds before republishing contents of a directory
+    time.sleep(2) # waiting 2 seconds before republishing contents of a directory
--- subscriber.py
+++ subscriber.py
@@ -1,19 +1,16 @@
-import zmq, csv
+import zmq, time
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB) # creating subscriber socket
 
-file = open('config.csv', 'r') # config file
-reader = csv.reader(file)
-configs = [(ip, port) for row, (ip, port) in enumerate(reader) if row == 1]
+configs = open('config.txt', 'r') # config file
 
-ip = configs[0][0] # host ip
-port = configs[0][1] # what port to use
+ip = configs.readline().replace('\n', '') # host ip
+port = configs.readline() # what port to use
 p = 'tcp://' + ip + ':' + port # how to communicate
 
 socket.connect(p) # connecting to the server
 socket.setsockopt_string(zmq.SUBSCRIBE, 'Folder') # subscribing to 'Folder' messages
-
 
 while True:
     content = socket.recv_string() # receive from publisher
